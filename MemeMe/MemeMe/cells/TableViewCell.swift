579bc1bfa1023f0fa68c1fe9120b6e5774965b43
Ramiz Raja <informramiz@gmail.com>
2019-05-18 20:39:10 +0500

    feat: Connect prototype table cell to class

---
+++ MemeMe/MemeMe/cells/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  MemeMe
+//
+//  Created by Apple on 18/05/2019.
+//  Copyright © 2019 RR Inc. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
